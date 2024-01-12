bf6ca29337e054f9322bc38093bb2d2ee0d71882
mario <mariogolemovic12@gmail.com>
2024-01-12 15:37:00 +0100

    popravak

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ import UpdatePlayer from '../views/UpdatePlayerView.vue'
 import DetailsPlayer from '../views/DetailsPlayerView.vue'
 import Players from '../views/PlayersView.vue'
 import Gallery from '../views/GalleryView.vue'
-import Gallery2 from '../views/GalleryView2.vue'
+
 
 
 const router = createRouter({
@@ -23,11 +23,7 @@ const router = createRouter({
       name: 'Home',
       component: Home
     },
-    {
-      path: '/gallery2',
-      name: 'Gallery2',
-      component: Gallery2
-    },
+   
     {
       path: '/gallery',
       name: 'Gallery',
